dd12d03e2a5f632ecd43e71d6ac9c94630e84b3c
Nico8287 <nico8287@gmail.com>
2013-09-13 14:02:17 -0300

    Todo

---
+++ src/Escuala/UsuarioBundle/Entity/Usuario.php
@@ -0,0 +1,27 @@
+<?php
+// src/Acme/UserBundle/Entity/User.php
+
+namespace Escuala\UsuarioBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="usuarios")
+ */
+class Usuario extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
+}
---
+++ src/Escuala/UsuarioBundle/EscuelaUsuarioBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Escuala\UsuarioBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class EscuelaUsuarioBundle extends Bundle
+{
+}
